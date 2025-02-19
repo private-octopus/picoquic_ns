54794178f220c71ec2a4495f1b4a439dfdb88eb7
Christian Huitema <huitema@huitema.net>
2025-02-19 13:31:21 -0800

    Try different order of libraries

--- cmake/FindPicoquic.cmake
+++ cmake/FindPicoquic.cmake
@@ -44,10 +44,10 @@ find_package_handle_standard_args(Picoquic REQUIRED_VARS
 
 if(Picoquic_FOUND)
     set(Picoquic_LIBRARIES
-        ${Picoquic_CORE_LIBRARY}
-        ${Picoquic_LOG_LIBRARY}
         ${Picoquic_TEST_LIBRARY} 
         ${Picoquic_HTTP_LIBRARY}
+        ${Picoquic_LOG_LIBRARY} 
+        ${Picoquic_CORE_LIBRARY}
     )
     set(Picoquic_INCLUDE_DIRS
         ${Picoquic_INCLUDE_DIR}
