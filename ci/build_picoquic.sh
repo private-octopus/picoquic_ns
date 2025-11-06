d9068fd59f7081cfeb52f3b975fceea34f1de870
huitema <huitema@huitema.net>
2025-11-05 16:06:50 -0800

    Update picoquic.sh to get submodules

--- ci/build_picoquic.sh
+++ ci/build_picoquic.sh
@@ -3,7 +3,11 @@
 
 cd ..
 git clone https://github.com/private-octopus/picoquic
+
 cd picoquic
+git checkout
+git submodule init
+git submodule update
 cmake $CMAKE_OPTS .
 make -j$(nproc) all
 cd ..
